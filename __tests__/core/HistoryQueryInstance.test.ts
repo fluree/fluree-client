10b0ea91c0def1d12167276f6b6b42684f5418ba
Jack White <jwhite5672@gmail.com>
2024-03-06 14:12:27 -0500

    Copied over methods and tests from QueryInstance to HistoryQueryInstance to acheive feature partity between the two.

--- __tests__/core/HistoryQueryInstance.test.ts
+++ __tests__/core/HistoryQueryInstance.test.ts
@@ -1,5 +1,7 @@
 import { FlureeClient } from '../../src';
 import { FlureeError } from '../../src/core/FlureeError';
+import { verifyJWS } from '@fluree/crypto';
+import { v4 as uuid } from 'uuid';
 
 describe('HistoryQueryInstance', () => {
   it('can send a history query', async () => {
@@ -67,5 +69,137 @@ describe('HistoryQueryInstance', () => {
     if (error instanceof FlureeError) {
       expect(error.message).toMatch(/db\/invalid-query/);
     }
+  });
+
+
+  it('can use sign() to sign a history query', async () => {
+    if (!process.env.TEST_PRIVATE_KEY) {
+      fail('TEST_PRIVATE_KEY not defined');
+    }
+    if (!process.env.TEST_DID) {
+      fail('TEST_DID not defined');
+    }
+    const client = await new FlureeClient({
+      host: process.env.FLUREE_CLIENT_TEST_HOST,
+      port: Number(process.env.FLUREE_CLIENT_TEST_PORT),
+      ledger: 'fluree-client/sign-function-history',
+      create: true,
+      privateKey: process.env.TEST_PRIVATE_KEY,
+    }).connect();
+    const did = client.getDid();
+    if (!did) {
+      fail('DID not defined');
+    }
+
+    await client
+      .transact({
+        '@context': {
+          f: 'https://ns.flur.ee/ledger#',
+        },
+        insert: [
+          {
+            '@id': 'ex:rootPolicy',
+            '@type': ['f:Policy'],
+            'f:targetNode': { '@id': 'f:allNodes' },
+            'f:allow': [
+              {
+                '@id': 'ex:rootAccessAllow',
+                'f:targetRole': { '@id': 'ex:rootRole' },
+                'f:action': [{ '@id': 'f:view' }, { '@id': 'f:modify' }],
+              },
+            ],
+          },
+          {
+            '@id': did,
+            'f:role': { '@id': 'ex:rootRole' },
+          },
+        ],
+      })
+      .send();
+    const response = await client
+      .history({
+        "commit-details": true,
+        t: { at: 'latest' }
+      })
+      .sign()
+      .send();
+    expect(response).toBeDefined();
+  });
+
+  it('can sign messages by default if config.signMessages is true', async () => {
+    if (!process.env.TEST_PRIVATE_KEY) {
+      fail('TEST_PRIVATE_KEY not defined');
+    }
+    if (!process.env.TEST_DID) {
+      fail('TEST_DID not defined');
+    }
+    const client = await new FlureeClient({
+      host: process.env.FLUREE_CLIENT_TEST_HOST,
+      port: Number(process.env.FLUREE_CLIENT_TEST_PORT),
+      ledger: 'fluree-client/sign-messages-history',
+      create: true,
+      privateKey: process.env.TEST_PRIVATE_KEY,
+    }).connect();
+
+    client.configure({ signMessages: true }); // If this is set, messsages are signed by default
+
+    const signedQuery = client
+      .history({
+        history: 'andrew',
+        t: { at: 'latest' }
+      })
+      .getSignedQuery();
+
+    const verificationResult = verifyJWS(JSON.parse(signedQuery));
+
+    let pubkey, payload;
+
+    if (verificationResult && verificationResult.arr) {
+      payload = verificationResult.arr[1];
+      pubkey = verificationResult.arr[3];
+    }
+
+    const publicKey = client.getPublicKey();
+
+    expect(pubkey).toBeDefined();
+    expect(pubkey).toBe(publicKey);
+    expect(payload).toBeDefined();
+  });
+
+  it('can return a query object', async () => {
+    const client = await new FlureeClient({
+      host: process.env.FLUREE_CLIENT_TEST_HOST,
+      port: Number(process.env.FLUREE_CLIENT_TEST_PORT),
+      ledger: uuid(),
+      create: true,
+    }).connect();
+
+    const query = client.query({
+      select: { freddy: ['*'] },
+    });
+    const queryObject = query.getQuery();
+    expect(queryObject).toBeDefined();
+    expect(queryObject).toMatchObject({ select: { freddy: ['*'] } });
+  });
+
+  it('throws an error if sign() is called without any privateKey', async () => {
+    const client = await new FlureeClient({
+      host: process.env.FLUREE_CLIENT_TEST_HOST,
+      port: Number(process.env.FLUREE_CLIENT_TEST_PORT),
+      ledger: 'fluree-client/sign-without-key-error-query-history',
+      create: true,
+    }).connect();
+
+    let error;
+    try {
+      client
+        .query({
+          selectOne: { andrew: ['*'] },
+        })
+        .sign();
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toBeDefined();
   });
 });
