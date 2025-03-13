03fbb56d57e55038c303ba43179a13828aa4afc1
Andrew Johnson <ajohnson.uva@gmail.com>
2025-03-12 23:21:02 -0400

    include interfaces in library export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { FlureeClient } from './core/FlureeClient';
 export default FlureeClient;
-export { FlureeClient };
+import { IFlureeConfig } from './interfaces/IFlureeConfig';
+import { IFlureeQuery } from './interfaces/IFlureeQuery';
+import { IFlureeTransaction } from './interfaces/IFlureeTransaction';
+import { IFlureeHistoryQuery } from './interfaces/IFlureeHistoryQuery';
+export {
+  FlureeClient,
+  IFlureeConfig,
+  IFlureeQuery,
+  IFlureeTransaction,
+  IFlureeHistoryQuery,
+};
